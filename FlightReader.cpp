c079b8bb64cdbab150aff410081ca4ccba9cef46
Gal Marcovich <gal16m@gmail.com>
2018-12-18 01:37:07 +0200

    עשינו את הלאקסר שמפריד לפי רווח ופסיק

--- FlightReader.cpp
+++ FlightReader.cpp
@@ -20,17 +20,25 @@ void FlightReader::lexer(string line) {
     for (int i = 0; i < line.size(); ++i) {
         //char s = line.at(i);
         if (isOperator(line.at(i))) {
-            buffer += space + line.at(i);
+            if (buffer[buffer.size() -1] != ' ') {
+                buffer += space;
+            }
+            buffer += line.at(i);
             if ((to_string(line.at(i + 1)) != space) && (i != (line.size() - 1))) {
                 buffer += space;
             }
         } else if (line.at(i) == ',') {
-            buffer += space;
+            if (buffer[buffer.size() -1] != ' ') {
+                buffer += space;
+            }
+            if (line.at(i + 1) == '-') {
+                buffer += '0';
+            }
         } else {
             buffer += line.at(i);
         }
     }
-    //cout << buffer << endl;
+    cout << buffer << endl;
 
     // split each line it get from the file to list of separate strings
     vector<string> info;
@@ -41,20 +49,63 @@ void FlightReader::lexer(string line) {
         buffer.erase(0, pos + delimiter.length());
     }
     info.push_back(buffer.substr(0, pos));
-    parser(info);
+    vector<string> result;
+    result = uniteParam(info);
+    parser(result);
 }
 
 vector<string> FlightReader::uniteParam(vector<string> info) {
     vector<string> params;
     string param1;
-    for (int i = 0; i < info.size(); ++i) {
-        if ((info[i]) == "(") {
-            while((info[i]) != ")") {
+    int i = 0;
+    int howManyBrackets = 0;
+    while (i < info.size()) {
+        if(!isOperator((info[i]).at(0))) {
+            if (i == (info.size() - 1)) {
+                param1 += info[i];
                 params.push_back(param1);
+                param1 = "";
+                ++i;
+                continue;
+            } else if (!isOperator((info[i + 1]).at(0))) {
+                param1 += info[i];
+                params.push_back(param1);
+                param1 = "";
+                ++i;
+                continue;
+            } else if (info[i + 1] == "(") {
+                param1 += info[i];
+                params.push_back(param1);
+                param1 = "";
+                ++i;
+                continue;
+            } else if ((i != 0) && (info[i - 1] == ")")) {
+                params.push_back(param1);
+                param1 = "";
             }
         }
-
+        if (info[i] != "(") {
+            param1 += info[i];
+        }
+        if (info[i] == "(") {
+            howManyBrackets++;
+            param1 += info[i];
+            ++i;
+            while (howManyBrackets != 0) {
+                if (info[i] == "(") {
+                    howManyBrackets++;
+                }
+                if (info[i] == ")") {
+                    howManyBrackets--;
+                }
+                param1 += info[i];
+                ++i;
+            }
+            continue;
+        }
+        ++i;
     }
+    return params;
 }
 
 void FlightReader::parser(vector<string> info) {
